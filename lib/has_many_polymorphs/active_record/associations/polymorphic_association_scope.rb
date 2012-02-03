51c415164e55b7be74bc72dce56a559c88e78cbc
uzzz <uzzable@gmail.com>
2012-02-03 10:43:45 +0300

    polymorphic association now takes into account default scopes if models when building joins; version bump 1.0.2

--- lib/has_many_polymorphs/active_record/associations/polymorphic_association_scope.rb
+++ lib/has_many_polymorphs/active_record/associations/polymorphic_association_scope.rb
@@ -24,19 +24,50 @@ module ActiveRecord
 
       def construct_joins(custom_joins = nil) #:nodoc:
         # build the string of default joins
-        "JOIN #{construct_owner} AS polymorphic_parent ON #{construct_from}.#{options[:foreign_key]} = polymorphic_parent.#{construct_owner_key} " +
+        "JOIN #{construct_owner} AS polymorphic_parent " +
+          "ON #{construct_from}.#{options[:foreign_key]} = " +
+            "polymorphic_parent.#{construct_owner_key} " +
         options[:from].map do |plural|
-          klass = plural.to_s.classify.constantize
-          "LEFT JOIN #{klass.quoted_table_name} " +
-          "ON #{construct_from}.#{options[:polymorphic_key]} = #{klass.quoted_table_name}.#{klass.primary_key} " +
-          "AND #{construct_from}.#{options[:polymorphic_type_key]} = #{@association.reflection.klass.quote_value(klass.base_class.name)}"
-        end.uniq.join(" ")# + " #{custom_joins}"
+          klass = plural._as_class
+          join =
+            "LEFT JOIN #{klass.quoted_table_name} " +
+            "ON #{construct_from}.#{options[:polymorphic_key]} = " +
+              "#{klass.quoted_table_name}.#{klass.primary_key} " +
+            "AND #{construct_from}.#{options[:polymorphic_type_key]} = " +
+              "#{@association.reflection.klass.quote_value(klass.base_class.name)}"
+
+            unless (conditions = default_where(klass)).blank?
+              join += " AND #{conditions}"
+            end
+
+            join
+        end.uniq.join(" ")
+      end
+
+      # get where values from models' default scopes
+      # and build sql
+      def default_where(klass)
+        sql          = ''
+        arel_nodes   = klass.scoped.with_default_scope.where_values.grep(Arel::Nodes::Equality)
+        string_nodes = klass.scoped.with_default_scope.where_values.grep(String)
+
+        unless arel_nodes.empty?
+          sql += arel_nodes.inject(arel_nodes[0]) do |arel, equality|
+            arel = arel.and(equality); arel
+          end.to_sql
+        end
+
+        unless string_nodes.empty?
+          sql += ' AND ' + string_nodes.map { |node| "(#{node})"}.join(' AND ')
+        end
+
+        sql
       end
 
       def construct_quoted_owner_attributes(*args) #:nodoc:
         # no access to returning() here? why not?
         type_key = options[:foreign_type_key]
-        h = {options[:foreign_key] => owner.id}
+        h = { options[:foreign_key] => owner.id }
         h[type_key] = owner.class.base_class.name if type_key
         h
       end
--- lib/has_many_polymorphs/version.rb
+++ lib/has_many_polymorphs/version.rb
@@ -1,3 +1,3 @@
 module HasManyPolymorphs
-  VERSION = '1.0.1'
+  VERSION = '1.0.2'
 end
